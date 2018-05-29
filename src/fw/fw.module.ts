7b9f13f84e68f741666007f697f98e0a8093b1f5
James Morris <blondfro@gmail.com>
2018-05-29 15:41:11 -0500

    created a pop-up menu component, modified it's view to display the fw-menu and set up some styling rules.

--- src/fw/fw.module.ts
+++ src/fw/fw.module.ts
@@ -14,6 +14,7 @@ import {MenuService} from './services/menu.service';
 import {MenuComponent} from './menus/menu/menu.component';
 import {MenuItemComponent} from './menus/menu-item/menu-item.component';
 import {RouterModule} from '@angular/router';
+import {PopupMenuComponent} from './menus/popup-menu/popup-menu.component';
 
 @NgModule({
   imports: [
@@ -29,7 +30,8 @@ import {RouterModule} from '@angular/router';
     ScreenLargeDirective,
     ScreenBelowLargeDirective,
     MenuComponent,
-    MenuItemComponent
+    MenuItemComponent,
+    PopupMenuComponent,
   ],
   providers: [
     FrameworkConfigService,
