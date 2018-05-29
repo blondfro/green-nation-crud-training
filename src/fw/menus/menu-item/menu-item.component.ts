2ace316450b7db800d5e6c90df9780bb551ceb61
James Morris <blondfro@gmail.com>
2018-05-29 16:32:28 -0500

    added in mouse handling events and methods to be able to display the popup menus when the mouse enters and leaves.

--- src/fw/menus/menu-item/menu-item.component.ts
+++ src/fw/menus/menu-item/menu-item.component.ts
@@ -1,5 +1,6 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostBinding, HostListener, Input, OnInit} from '@angular/core';
 import {MenuItem, MenuService} from '../../services/menu.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'fw-menu-item',
@@ -9,10 +10,49 @@ import {MenuItem, MenuService} from '../../services/menu.service';
 export class MenuItemComponent implements OnInit {
 
   @Input() item: MenuItem;
+  @HostBinding('class.parent-is-popup')
+  @Input() parentIsPopup = true;
+  isActiveRoute = false;
 
-  constructor(private menuService: MenuService) { }
+  mouseInPopup = false;
+  mouseInItem = false;
+  popupLeft = 0;
+  popupTop = 34;
+
+  constructor(private menuService: MenuService,
+              private router: Router) { }
 
   ngOnInit() {
   }
 
+  onPopupMouseEnter(event): void {
+    if (!this.menuService.isVertical) {
+      this.mouseInPopup = true;
+    }
+  }
+
+  onPopupMouseLeave(event): void {
+    if (!this.menuService.isVertical) {
+      this.mouseInPopup = false;
+    }
+  }
+
+  @HostListener('mouseleave', ['$event'])
+  onMouseLeave(event): void {
+    if (!this.menuService.isVertical) {
+      this.mouseInItem = false;
+    }
+  }
+
+  @HostListener('mouseenter') onMouseEnter(): void {
+    if (!this.menuService.isVertical) {
+      if (this.item.submenu) {
+        this.mouseInItem = true;
+        if (this.parentIsPopup) {
+          this.popupLeft = 160;
+          this.popupTop = 0;
+        }
+      }
+    }
+  }
 }
