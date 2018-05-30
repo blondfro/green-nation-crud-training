e49975b55ee41d5f1499579346f136646e816f34
James Morris <blondfro@gmail.com>
2018-05-30 15:41:12 -0500

    modified the app module to import the user service and the user api.
    created an on submit method for the sign-in component.

--- src/fw/users/sign-in/sign-in.component.ts
+++ src/fw/users/sign-in/sign-in.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {UserApi} from '../user-api';
+import {Router} from '@angular/router';
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'fw-sign-in',
@@ -11,7 +13,31 @@ export class SignInComponent implements OnInit {
   formError: string;
   submitting = false;
 
-  constructor(private userApi: UserApi) { }
+  constructor(private userApi: UserApi,
+              private router: Router) {
+
+  }
+
+  onSubmit(signInForm: NgForm) {
+    if (signInForm.valid) {
+      console.log('submitting...', signInForm);
+      this.submitting = true;
+      this.formError = null;
+
+      this.userApi.signIn(signInForm.value.username,
+        signInForm.value.password,
+        signInForm.value.rememberMe)
+        .subscribe((data) => {
+          console.log('got valid: ', data);
+          this.router.navigate(['/authenticated']);
+        },
+          (err) => {
+          this.submitting = false;
+          console.log('got error: ', err);
+          this.formError = err;
+          });
+    }
+  }
 
   ngOnInit() {
   }
